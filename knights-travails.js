c7c98ad8d176b29b5ad4e874b4d811a5c853ba94
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-10-21 17:44:22 +0700

    before refactoring again

--- knights-travails.js
+++ knights-travails.js
@@ -1,51 +1,52 @@
-let m = 8
-let n = 8
-let grid = []
-let queue = [];
-
-
-let possibleKnightMoves = [[1,2],[2,1],[2,-1],[1,-2],[-1,2],[-2,1],[-2,-1],[-1,-2]]
-
-for(let i = 0 ; i < m; i++){
-    grid.push([]);
-    for(let j = 0; j<n; j++){
-        grid[i].push(0)
-    }
-}
 
 class Node{
     constructor(coords){
-        [this.coordsX,this.coordsY] = [...coords];
+        [this.currentCoordX, this.currentCoordY] = [...coords];
         this.child = [];
     }
 }
 
-function knightMoves(start,end){
-    let [targetPosX, targetPosY] = [...end];
-    let length = 0;
-    let visited = [];
+function knightMoves(startCoordinates,endCoordinates){
+    let [endCoordinateX, endCoordinateY] = [...endCoordinates];
+    let step = 0;
+    let visitedNodesArray = [];
+    let queueArray = [];
+    let possibleKnightMoves = [[1,2],[2,1],[2,-1],[1,-2],[-1,2],[-2,1],[-2,-1],[-1,-2]];
 
-    queue.push(new Node(start));
-    visited.push(start); // check if visited already or not;
-    while(queue.length>0 ){
-        for(item of queue){
-            let visitedNode = queue.unshift();
-            visited.push([visitedNode.coordsX,visitedNode.coordsY]);
+    let m = 8;
+    let n = 8;
+    let grid = [];
+    
+    
+    for(let i = 0 ; i < m; i++){
+        grid.push([]);
+        for(let j = 0; j<n; j++){
+            grid[i].push(0);
+        }
+    }
+    
+    //initialize queue (line 28 29)
+    queueArray.push(new Node(startCoordinates));
+    visitedNodesArray.push(startCoordinates); // to check if a coordinates already visited or not.
+    while(queueArray.length > 0 && queueArray[0].currentCoordX != endCoordinateX && queueArray[0].currentCoordY != endCoordinateY){
+    let visitedNode = queueArray.unshift();
+    if(visitedNode.currentCoordX == endCoordinateX && visitedNode.currentCoordY == endCoordinateY){return step};     
+        
+        
+        for(item of queueArray){
+            let visitedNode = queueArray.unshift();
+            visitedNodesArray.push([visitedNode.currentCoordX,visitedNode.currentCoordY]);
             for(dir of possibleKnightMoves){
-                let nextMoveCoords = [visitedNode.coordsX+dir[0],visitedNode.coordsY+dir[1]]  
-                if(visited.some(arr=> arr.toString() === nextMoveCoords.toString())){continue}; //if visited, continue loop
-                if(nextMoveCoords[0]==targetPosX && nextMoveCoords[1]==targetPosY){
-                    visitedNode = new Node(nextMoveCoords[0],nextMoveCoords[1]);
-                    length += 1; 
-                    return length};
-                // queue[0].child.push(new Node(nextMoveCoords));
-                // queue.push(queue[0].child);
-                queue.push(new Node(nextMoveCoords));
+                let nextMoveCoords = [visitedNode.currentCoordX+dir[0],visitedNode.currentCoordY+dir[1]];
+                if(nextMoveCoords[0] < -2 || nextMoveCoords[1] < -2 || nextMoveCoords[0] > m || nextMoveCoords[1] > n){continue}  
+                if(visitedNodesArray.some(arr=> arr.toString() === nextMoveCoords.toString())){continue}; //if visitedNodesArray, continue loop
+                if(visitedNode.currentCoordX==endCoordinateX && visitedNode.currentCoordY==endCoordinateY){return step};
+                queueArray.push(new Node(nextMoveCoords));
             }
         }
-        length = length + 1
+        step = step + 1
     }
-    return length;
+    return step;
 }
 
-console.log(knightMoves([0,0],[3,3]))
+console.log(knightMoves([0,0],[1,2]))
